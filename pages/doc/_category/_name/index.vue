9057db674ec15a3123404c6368554f8aa8d22b76
이태규 <koallazon@gmail.com>
2019-12-16 00:45:41 +0900

    listoutput_move

---
+++ pages/doc/_category/_name/index.vue
@@ -0,0 +1,19 @@
+<template>
+    <v-card>{{ doc }}</v-card>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      doc: ''
+    }
+  },
+  async mounted () {
+    console.log(this.$route.params.category)
+    const id = this.$route.params.category + '-' + this.$route.params.name
+    const sn = await this.$fireStore.collection('docs').doc(id).get()
+    this.doc = sn.data()
+  }
+}
+</script>
