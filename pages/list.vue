f412ea2564314b103221ae6ca718b84a965e36bc
koallazon <koallazon@gmail.com>
2019-12-24 13:00:11 +0900

    paging

--- pages/list.vue
+++ pages/list.vue
@@ -1,35 +1,45 @@
 <template>
     <v-container>
         <v-card>
-            <v-toolbar dense color="success" dark flat>
+            <v-toolbar color="success" dark flat>
                 <v-toolbar-title>list</v-toolbar-title>
-                <v-spacer></v-spacer>
+                <v-spacer />
+                <v-sheet width="100" color="transparent">
+                  <v-select
+                    v-model="sortName"
+                    :items="['date','title']"
+                    hide-details
+                    solo-inverted
+                    flat
+                    >
+                  </v-select>
+                </v-sheet>
                 <v-btn icon @click="list">
                     <v-icon>mdi-refresh</v-icon>
                 </v-btn>
             </v-toolbar>
             <v-card-text>
-              <v-list-item v-for="item in items" three-line :key="item.id">
-                <v-list-item-content>
-                  <v-list-item-title>{{item.title}}</v-list-item-title>
-                  <v-list-item-subtitle>{{item.description}}</v-list-item-subtitle>
-                  <v-list-item-subtitle>{{item.date}}</v-list-item-subtitle>
-                </v-list-item-content>
-                <v-list-item-action>
-                  <v-btn :to="`/doc/${item.category}/${item.name}`" icon>
-                    <v-icon>mdi-arrow-right</v-icon>
-                  </v-btn>
-                </v-list-item-action>
-              </v-list-item>
+              <template v-for="(item, i) in items">
+                <v-list-item :key="item.id" three-line>
+                  <v-list-item-content>
+                    <v-list-item-title>{{item.title}}</v-list-item-title>
+                    <v-list-item-subtitle>{{item.description}}</v-list-item-subtitle>
+                    <v-list-item-subtitle>{{item.date}}</v-list-item-subtitle>
+                  </v-list-item-content>
+                  <v-list-item-action>
+                    <v-btn :to="`/doc/${item.category}/${item.name}`" icon>
+                      <v-icon>mdi-arrow-right</v-icon>
+                    </v-btn>
+                  </v-list-item-action>
+                </v-list-item>
+                <v-divider :key="i" />
+              </template>
             </v-card-text>
-            <v-radio-group v-model="radioGroup">
-              <v-radio
-                v-for="n in 3"
-                :key="n"
-                :label="`Radio ${n}`"
-                :value="n"
-              ></v-radio>
-            </v-radio-group>
+          <v-card-actions>
+            <v-btn @click="next" text>
+              more
+            </v-btn>
+          </v-card-actions>
         </v-card>
     </v-container>
 </template>
@@ -39,7 +49,13 @@ export default {
   data () {
     return {
       items: [],
-      radioGroup: 1
+      last: null,
+      sortName: 'date'
+    }
+  },
+  watch: {
+    sortName () {
+      this.list()
     }
   },
   mounted () {
@@ -47,7 +63,11 @@ export default {
   },
   methods: {
     async list () {
-      const sn = await this.$fireStore.collection('docs').get()
+      this.items = []
+      const sn = await this.$fireStore.collection('docs')
+        .orderBy(this.sortName, 'desc')
+        .limit(3)
+        .get()
       sn.docs.forEach((v) => {
         const item = v.data()
         item.id = v.id
@@ -55,6 +75,22 @@ export default {
         item.name = v.id.split('-')[1]
         this.items.push(item)
       })
+      this.last = sn.docs[sn.docs.length - 1]
+    },
+    async next () {
+      const sn = await this.$fireStore.collection('docs')
+        .orderBy(this.sortName, 'desc')
+        .startAfter(this.last)
+        .limit(3)
+        .get()
+      sn.docs.forEach((v) => {
+        const item = v.data()
+        item.id = v.id
+        item.category = v.id.split('-')[0]
+        item.name = v.id.split('-')[1]
+        this.items.push(item)
+      })
+      this.last = sn.docs[sn.docs.length - 1]
     }
   }
 }
