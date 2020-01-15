882bffd46590b2f57fee8734e59ed4d9817b59f2
koallazon <koallazon@gmail.com>
2020-01-15 12:58:21 +0900

    writeAuth_login&logout

--- pages/write.vue
+++ pages/write.vue
@@ -1,25 +1,45 @@
 <template>
-    <v-container>
-        <v-card>
+    <v-container fluid>
+        <v-card v-if="user">
             <v-toolbar dense color="success" dark flat>
-                <v-toolbar-title>write</v-toolbar-title>
+                <v-toolbar-title>write {{user.email}}</v-toolbar-title>
                 <v-spacer></v-spacer>
                 <v-btn icon @click="save">
                     <v-icon>mdi-content-save</v-icon>
                 </v-btn>
+                <v-btn icon @click="signOut">
+                    <v-icon>mdi-account-box</v-icon>
+                </v-btn>
             </v-toolbar>
             <v-form>
                 <v-card-text>
-                    <v-text-field v-model="docId.category" label='category' />
-                    <v-text-field v-model="docId.name" label='name' />
-                    <v-divider />
-                    <v-text-field v-model="doc.title" label='title' />
-                    <v-text-field v-model="doc.date" label='date' />
-                    <v-text-field v-model="doc.description" label='description' />
-                    <v-divider />
-                    <v-textarea v-model="content.text" label="content" />
+                    <v-row>
+                        <v-col cols="12" sm="6">
+                          <v-text-field v-model="docId.category" label='category' outlined/>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-text-field v-model="docId.name" label='name' outlined/>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-text-field v-model="doc.title" label='title' outlined/>
+                        </v-col>
+                        <v-col cols="12" sm="6">
+                          <v-text-field v-model="doc.date" label='date' outlined/>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-text-field v-model="doc.description" label='description' outlined/>
+                        </v-col>
+                        <v-col cols="12">
+                          <v-textarea v-model="content.text" label="content" outlined/>
+                        </v-col>
+                    </v-row>
                 </v-card-text>
             </v-form>
+        </v-card>
+        <v-card v-else>
+          <v-card-text class="text-center">
+            <v-btn @click="signIn">signIn</v-btn>
+          </v-card-text>
         </v-card>
     </v-container>
 </template>
@@ -43,7 +63,8 @@ export default {
         createAt: new Date(),
         modifiedAt: new Date(),
         text: ''
-      }
+      },
+      user: null
     }
   },
   methods: {
@@ -56,6 +77,29 @@ export default {
       this.content.modifiedAt = new Date()
       await this.$fireStore.collection('docs').doc(cid).set(this.content)
       alert('저장완료')
+    },
+    async signIn () {
+      try {
+        this.loading = true
+        const provider = new this.$fireAuthObj.GoogleAuthProvider()
+        const auth = await this.$fireAuth.signInWithPopup(provider)
+        this.user = auth.user
+      } catch (e) {
+        console.erroe(e.message)
+      } finally {
+        this.loading = false
+      }
+    },
+    async signOut () {
+      try {
+        this.loading = true
+        await this.$fireAuth.signOut()
+        this.user = null
+      } catch (e) {
+        console.erroe(e.message)
+      } finally {
+        this.loading = false
+      }
     }
   }
 }
